3ab207ba1d7a15dbb7a7cbede247ea42b05acb44
jomorro <morrowjoe0@gmail.com>
2019-03-27 12:49:07 -0400

    Updates

---
+++ precache-manifest.4f7245ec27dc9b814dbfc88a708a2f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35cf09158f2f34069874",
+    "url": "/kwitters/static/css/main.3866e2af.chunk.css"
+  },
+  {
+    "revision": "35cf09158f2f34069874",
+    "url": "/kwitters/static/js/main.35cf0915.chunk.js"
+  },
+  {
+    "revision": "23ca896102ebbf56b903",
+    "url": "/kwitters/static/js/1.23ca8961.chunk.js"
+  },
+  {
+    "revision": "f93b9fe48f2bc24947bb",
+    "url": "/kwitters/static/js/runtime~main.f93b9fe4.js"
+  },
+  {
+    "revision": "34f3b9ef15e0669f103aea09a0b9b3fa",
+    "url": "/kwitters/static/media/favicon.34f3b9ef.ico"
+  },
+  {
+    "revision": "f411937cc08d7a2d3b30563e8c3ef1c2",
+    "url": "/kwitters/index.html"
+  }
+];
